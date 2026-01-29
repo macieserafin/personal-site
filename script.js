c85f3f78a892ad39b294eccb6893705daaa57ef2
Maciej Serafin <macieserafin@gmail.com>
2026-01-29 14:52:04 +0100

    Update portfolio content and projects section

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleMenu() {
     var menu = document.getElementById("nav-list");
     menu.classList.toggle("show");}
+
+//footer
+document.getElementById("year").textContent = new Date().getFullYear();
